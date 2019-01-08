6b613b04dbacb17ce661484178240a850a502230
choeffer <33625292+choeffer@users.noreply.github.com>
2019-01-08 15:23:01 +0100

    initial upload of the files

---
+++ README.rst
@@ -0,0 +1,138 @@
+py3syslog
+=========
+
+Python 3 implementation of a simple UDP syslog server which inserts the recieved 
+messages into a MariaDB_ or MySQL_ database.
+
+About
+-----
+
+The Python script will start a syslog server which inserts every recieved message 
+and write it into a MariaDB_ or MySQL_ database. The used timestamp is in UTC. 
+
+Every time the script is executed it will check if the database and table exist 
+and if not create them. Then the script is executed until it will be 
+manually closed. Every time a message is recieved on the defined port the 
+data will be decoded as UTF-8, inserted in the database and printed out in 
+the terminal.
+
+Be aware the that the UDP packages are not encypted. It is taken care off avoiding 
+SQL injections because the server might be facing the internet.
+
+The script uses the python module socketserver_ and the external module
+mysql-connector-python_ . See ``requirements.txt`` for installed packages and the 
+used versions. The file is created with ``pip3 freeze > requirements.txt``.
+
+Usage
+-----
+
+First install the required mysql-connector-python module in the global Python 3 
+environment or in a virtual Python 3 environment. The latter has the advantage that 
+the packages are isolated from other projects and also from the system wide 
+installed global once. If things get messed up, the virtual environment can 
+just be deleted and created from scratch again. For more informations about 
+virtual environments in Python 3, see venv1_ and venv2_ .
+
+.. code-block:: console
+
+    pip3 install mysql-connector-python
+
+Then modify the ``PORT``, ``db_name``, ``table_name``, ``db_user``, ``db_password``, 
+``db_host`` and ``db_port`` parameters in the ``syslogserver.py`` script and 
+execute it.
+
+.. code-block:: console
+
+    python3 syslogserver.py
+
+Or use e.g. tmux_ for leave it running in the background.
+
+Example Mikrotik RouterOS
+-------------------------
+
+The script was developed to recieve syslog messages from a MikroTik_ `wAP LTE kit`_ 
+and insert them into a MariaDB_ database to be able to display them via Grafana_ . 
+
+RouterOS
+^^^^^^^^
+
+The following settings are used in RouterOS. They need to be applied via CLI_ . 
+Change ``remote`` and ``remote-port`` to the one where the syslog server is 
+listening.
+
+.. code-block:: console
+
+    /system logging action
+    set 3 remote=123.123.123.123 remote-port=12312
+
+Next command just prints out the settings of ``/system logging action`` .
+
+.. code-block:: console
+
+    /system logging action> print
+    Flags: * - default 
+    0 * name="memory" target=memory memory-lines=1000 memory-stop-on-full=no 
+
+    1 * name="disk" target=disk disk-file-name="flash/log" 
+        disk-lines-per-file=1000 disk-file-count=2 disk-stop-on-full=no 
+
+    2 * name="echo" target=echo remember=yes 
+
+    3 * name="remote" target=remote remote=123.123.123.123 remote-port=12312 
+        src-address=0.0.0.0 bsd-syslog=no syslog-time-format=bsd-syslog 
+        syslog-facility=daemon syslog-severity=auto 
+
+It is also needed to add the remote syslog server as a destination for logs 
+on the wanted topics. The ``prefix=`` option is optional but useful to distinguish 
+between different devices as it is added in the logs which are sent to the remote 
+syslog server.
+
+.. code-block:: console
+
+    /system logging
+    add action=remote prefix=example_prefix topics=info
+    add action=remote prefix=example_prefix topics=error
+    add action=remote prefix=example_prefix topics=warning
+    add action=remote prefix=example_prefix topics=critical
+
+Grafana
+^^^^^^^
+
+In Grafana_ the database has to be added as a datasource_ . Then a table_ 
+with following SQL query in the Metrics tab can be added to a dashboard. The 
+SQL query has to be adjusted to the used database/table/columns structure, see 
+script ``syslogserver.py`` for more details how the database/table/columns are 
+created.
+
+.. code-block:: console
+
+    SELECT
+    timestamp,
+    message
+    FROM logging.logs
+    WHERE $__timeFilter(timestamp) AND message LIKE '%example_prefix%'
+    ORDER BY timestamp DESC
+
+Credits
+-------
+
+https://gist.github.com/marcelom/4218010 
+
+References
+----------
+
+.. target-notes::
+
+.. _MariaDB: https://mariadb.org/
+.. _MySQL: https://www.mysql.com/
+.. _socketserver: https://docs.python.org/3/library/socketserver.html
+.. _mysql-connector-python: https://pypi.org/project/mysql-connector-python/
+.. _venv1: https://docs.python.org/3/tutorial/venv.html
+.. _venv2: https://docs.python.org/3/library/venv.html
+.. _tmux: https://en.wikipedia.org/wiki/Tmux
+.. _Mikrotik: https://mikrotik.com/
+.. _`wAP LTE kit`: https://mikrotik.com/product/wap_lte_kit
+.. _Grafana: https://grafana.com/
+.. _CLI: https://wiki.mikrotik.com/wiki/Manual:First_time_startup#CLI
+.. _datasource: http://docs.grafana.org/features/datasources/mysql/
+.. _table: http://docs.grafana.org/features/panels/table_panel/
---
+++ syslogserver.py
@@ -0,0 +1,137 @@
+import socketserver
+from datetime import datetime
+import time
+
+import mysql.connector as mariadb
+
+#https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-transaction.html
+def write_to_db(sql_statement, cursor, debug = False, variables = None):
+
+    try:
+        if debug == True:
+            print('SQL statement: {}'.format(sql_statement))
+            print('Variables: {}'.format(variables))
+        #https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-execute.html
+        #this prevents SQL injections!!!
+        #http://bobby-tables.com/python
+        cursor.execute(operation = sql_statement, params = variables)
+
+        if debug == True:
+            print('The last inserted id was: {}'.format(cursor.lastrowid))
+        #https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlcursor-close.html
+        cursor.close()
+    except mariadb.Error as error:
+        print('Error: {}'.format(error))
+
+    return None
+
+def create_db(db_name, cursor, debug = False):
+
+    sql_statement = 'CREATE DATABASE IF NOT EXISTS {}'.format(db_name)
+
+    write_to_db(sql_statement, cursor, debug)
+
+    return None
+
+def create_table(db_name, table_name, cursor, debug = False):
+
+    #a separate id column as primary key is used to allow same timestamp values
+    #https://docs.python.org/3/reference/lexical_analysis.html#string-literal-concatenation
+    sql_statement = (
+        'CREATE TABLE IF NOT EXISTS {}.{}('
+        'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,'
+        'timestamp DATETIME,'
+        'message VARCHAR(128))').format(db_name, table_name)
+
+    write_to_db(sql_statement, cursor, debug)
+
+    return None
+
+def insert_data(db_name, table_name, cursor, data, debug=False):
+
+    #https://docs.python.org/3/reference/lexical_analysis.html#string-literal-concatenation
+    sql_statement = (
+        'INSERT INTO {}.{} SET '
+        'timestamp = %s,'
+        'message = %s').format(db_name, table_name)
+    variables = (datetime.utcnow(), data)
+
+    write_to_db(sql_statement, cursor, variables=variables)
+
+    return None
+
+#https://docs.python.org/3/library/socketserver.html
+class SyslogUDPHandler(socketserver.BaseRequestHandler):
+    """
+    Decodes syslog data and add timestamp.
+
+    This class takes the recieved syslog entries and writes them to the database.
+    """
+
+    def handle(self):
+
+        #open mariadb connection for creating database and table
+        try:
+            #https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
+            mariadb_connection = mariadb.connect(user = self.server.db_user, password = self.server.db_password, host = self.server.db_host, port = self.server.db_port)
+        except mariadb.Error as error:
+            print('Error: {}'.format(error))
+
+        data = bytes.decode(self.request[0].strip(), encoding="utf-8")
+
+        cursor = mariadb_connection.cursor()
+        insert_data(self.server.db_name, self.server.table_name, cursor, data)
+
+        #https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlconnection-commit.html
+        mariadb_connection.commit()
+
+        #https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlconnection-close.html
+        mariadb_connection.close()
+
+        print('At {} recieved following message: {}'.format(time.time(), data))
+
+if __name__ == "__main__":
+
+    HOST, PORT = "0.0.0.0", 12312
+
+    db_name = 'logging'
+    table_name = 'logs'
+    db_user = 'example_user'
+    db_password = 'example_password'
+    db_host = '192.168.312.312'
+    #default MariaDB port
+    db_port = 3306
+
+    #open mariadb connection for creating database and table
+    try:
+        #https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
+        mariadb_connection = mariadb.connect(user = db_user, password = db_password, host = db_host, port = db_port)
+    except mariadb.Error as error:
+        print('Error: {}'.format(error))
+
+    #every time a new cursor is created because the old one gets closed 
+    #in the called write_to_db function
+    cursor = mariadb_connection.cursor()
+    create_db(db_name, cursor)
+
+    cursor = mariadb_connection.cursor()
+    create_table(db_name, table_name, cursor)
+
+    #https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlconnection-commit.html
+    mariadb_connection.commit()
+
+    #https://dev.mysql.com/doc/connector-python/en/connector-python-api-mysqlconnection-close.html
+    mariadb_connection.close()
+
+    server = socketserver.UDPServer((HOST, PORT), SyslogUDPHandler)
+    #self defined variables which are needed in handle()
+    #https://stackoverflow.com/questions/6875599/with-python-socketserver-how-can-i-pass-a-variable-to-the-constructor-of-the-han
+    server.db_name = db_name
+    server.table_name = table_name
+    server.db_user = db_user
+    server.db_password = db_password
+    server.db_host = db_host
+    server.db_port = db_port
+
+    #handle requests until explicit shutdown(), see python docs
+    server.serve_forever()
